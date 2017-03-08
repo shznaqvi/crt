ec0e341adba03e1966ddd780b94b183c8eaec1c3
shznaqvi <shznaqvi@gmail.com>
2017-03-08 11:21:06 +0500

    Initial commit

--- app/src/main/java/edu/aku/hassannaqvi/crt/SplashscreenActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/crt/SplashscreenActivity.java
@@ -33,6 +33,20 @@ public class SplashscreenActivity extends Activity {
     private static final int UI_ANIMATION_DELAY = 300;
     private static int SPLASH_TIME_OUT = 3000;
     private final Handler mHideHandler = new Handler();
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
     private View mContentView;
     private final Runnable mHidePart2Runnable = new Runnable() {
         @SuppressLint("InlinedApi")
@@ -68,20 +82,6 @@ public class SplashscreenActivity extends Activity {
         @Override
         public void run() {
             hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
         }
     };
 
