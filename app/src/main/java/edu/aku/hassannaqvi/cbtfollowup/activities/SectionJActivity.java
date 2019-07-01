5a94839a3e04afed52f52057559be6c2aedb4c69
SAJID LATIF <indus1530@gmail.com>
2019-07-01 16:25:39 +0500

    J-Section layout

---
+++ app/src/main/java/edu/aku/hassannaqvi/cbtfollowup/activities/SectionJActivity.java
@@ -0,0 +1,15 @@
+package edu.aku.hassannaqvi.cbtfollowup.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.cbtfollowup.R;
+
+public class SectionJActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_j);
+    }
+}
